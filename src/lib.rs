b93836d895a5db685c02b4248e222407b7c862f0
zjp <jiping_zhou@foxmail.com>
2023-09-24 17:25:57 +0800

    add more tests: see the previous commit for viewing the tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-#![allow(clippy::missing_panics_doc, clippy::must_use_candidate)]
+#![allow(
+    clippy::missing_panics_doc,
+    clippy::must_use_candidate,
+    clippy::enum_glob_use
+)]
+
+#[doc(hidden)]
+pub mod doc;
 
 pub mod fetch;
 pub mod prettify;
